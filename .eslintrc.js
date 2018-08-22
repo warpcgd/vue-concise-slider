ae1b27cfca93c005f8e85f4687e0fbcb1d01c262
lixichen <lixichen@yy.com>
2018-08-22 14:44:54 +0800

    webpack 3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   devtool: process.env.NODE_ENV === 'production' ? '' : 'inline-source-map',
   devServer: { // 检测代码变化并自动重新编译并自动刷新浏览器
     contentBase: path.resolve(__dirname, 'dist'), // 设置静态资源的根目录
-    hot: true,
+    hot: true
   },
   entry: {
     module: './src/exportModule.js',
