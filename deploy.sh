6fbb83b1fe34e5dbc6e974775d6a67460c30cb9b
lixichen <lixichen@yy.com>
2018-12-21 17:58:50 +0800

    v4

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:<warpcgd>/<warpcgd>.github.io.git master
+# git push -f git@github.com:<warpcgd>/<warpcgd>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:warpcgd/vue-concise-slider.git master:gh-pages
 
 cd -
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  base: '/vue-concise-slider/',
   title: 'vue-concise-slider',
   description: 'vue-concise-slider,A simple sliding component,has easy configuration,support self-adaption / fullscreen / button / page,and is compatible with mobile and PC terminal',
   themeConfig: {
